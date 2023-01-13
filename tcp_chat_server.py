9254575ab37fd31aa2963696d92697c5b4279196
dithermedaddy <moi@faanross.com>
2023-01-13 08:08:54 -0500

    tcp chat client

--- tcp_chat_server.py
+++ tcp_chat_server.py
@@ -38,25 +38,3 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-# Receiving / Listening Function
-def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
-
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
-
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
