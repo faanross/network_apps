b1aa33902423f0cb4b838b214531a7bce727ddb5
dithermedaddy <moi@faanross.com>
2023-01-13 08:14:31 -0500

    tcp chat client

--- tcp_chat_client.py
+++ tcp_chat_client.py
@@ -1,2 +1,9 @@
 import socket
 import threading
+
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
