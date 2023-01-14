151f89bb3e2216c89cca04985032baf162fdfb0a
dithermedaddy <moi@faanross.com>
2023-01-14 09:56:18 -0500

    tcp client

--- tcp_client.py
+++ tcp_client.py
@@ -10,4 +10,4 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send some data once connected
-client.send(b"GET / HTTP/1.1")
+client.send(b"GET / HTTP/1.1\r\n")
