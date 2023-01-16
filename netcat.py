e4629d256179ce575a8a6f4f28efebd6311f934a
dithermedaddy <moi@faanross.com>
2023-01-16 13:16:58 -0500

    netcat

--- netcat.py
+++ netcat.py
@@ -28,6 +28,6 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', action='store_true', help='command shell')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
     parser.add_argument('-t', '--target', action='store_true', help='command shell')
     parser.add_argument('-u', '--upload', action='store_true', help='command shell')
