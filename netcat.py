8492c9ead990da64926706440aeb8dc7340988c6
dithermedaddy <moi@faanross.com>
2023-01-16 13:07:14 -0500

    netcat

--- netcat.py
+++ netcat.py
@@ -10,4 +10,6 @@ def execute(cmd):
     cmd - cmd.strip()
     if not cmd:
         return
-        
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
+
