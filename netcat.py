0e723c5f1ecc40bee8929e8872847b8cebb63af6
dithermedaddy <moi@faanross.com>
2023-01-16 13:12:42 -0500

    netcat

--- netcat.py
+++ netcat.py
@@ -18,6 +18,9 @@ if __name__ == '__main__':
         description='BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''Example:
-            net
-
-
+            netcat.py -t 192.16.1.108 -p 5555 -l -c #command shell
+            netcat.py -t 192.16.1.108 -p 5555 -l -u=myteest.text #upload file
+            netcat.py -t 192.16.1.108 -p 5555 -l -e=\"cat /etc/passwd\" #execute command
+            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 #echo text to server port 135
+            netcat.py -t 192.16.1.108 -p 5555 #connect to server
+        '''))
