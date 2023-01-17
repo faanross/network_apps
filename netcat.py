89bf904d4e21828cdc33cc05d97dd60e551bf379
dithermedaddy <moi@faanross.com>
2023-01-17 10:01:00 -0500

    netcat continue...

--- netcat.py
+++ netcat.py
@@ -34,6 +34,12 @@ class Netcat:
                 while True:
                     recv_len = 1
                     response = ''
+                    while recv_len:
+                        data = self.socket.recv(4096)
+                        recv_len = len(data)
+                        response += data.decode()
+                        if recv_len < 4096:
+                            break
 
 
 
