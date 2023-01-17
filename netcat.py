9801c874f83d54f21b916d678d3bbcfa7f61c232
dithermedaddy <moi@faanross.com>
2023-01-17 09:49:00 -0500

    netcat continue...

--- netcat.py
+++ netcat.py
@@ -17,6 +17,7 @@ class Netcat:
     def __init__(self,args,buffer=None):
         self.args = args
         self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
 
 
