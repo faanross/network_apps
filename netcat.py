10d6e07075d6fba86b4b61f79026a68b23ae6d86
dithermedaddy <moi@faanross.com>
2023-01-19 07:43:54 -0500

    ssh client

--- netcat.py
+++ netcat.py
@@ -18,7 +18,7 @@ class NetCat:
         self.args = args
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def run(self):
         if self.args.listen:
