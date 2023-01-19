824e8b890e12c251c102f84aba80261e7c52d3ac
dithermedaddy <moi@faanross.com>
2023-01-19 07:45:40 -0500

    ssh client

--- ssh_client.py
+++ ssh_client.py
@@ -3,4 +3,5 @@ import paramiko
 def ssh_command(ip, port, user, passwd, cmd):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
     
