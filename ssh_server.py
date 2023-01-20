b92c1b4379efa625964d15c57212adf52412c742
dithermedaddy <moi@faanross.com>
2023-01-20 07:51:13 -0500

    ssh server

--- ssh_server.py
+++ ssh_server.py
@@ -7,3 +7,6 @@ import threading
 CWD = os.path.dirname(os.path.realpath(__file__))
 HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, '.test_rsa.key'))
 
+class Server (paramiko.ServerInterface):
+    def __init__(self):
+        
