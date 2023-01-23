b0971d201fdd3bb5a588c4c5c852127a9683870a
dithermedaddy <moi@faanross.com>
2023-01-23 15:42:35 -0500

    ssh rcmd

--- ssh_rcmd.py
+++ ssh_rcmd.py
@@ -5,4 +5,9 @@ import subprocess
 def ssh_command(ip, port, user, passwd, command):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
+    client.connect(ip, port=port, username=user, password=passwd)
+
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        
