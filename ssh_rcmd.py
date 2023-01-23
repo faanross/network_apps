527133b6d701f51264da5501870b42dec044f53d
dithermedaddy <moi@faanross.com>
2023-01-23 15:43:41 -0500

    ssh rcmd

--- ssh_rcmd.py
+++ ssh_rcmd.py
@@ -10,4 +10,9 @@ def ssh_command(ip, port, user, passwd, command):
     ssh_session = client.get_transport().open_session()
     if ssh_session.active:
         ssh_session.send(command)
-        
+        print(ssh_session.recv(1024).decode())
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode()
+                
