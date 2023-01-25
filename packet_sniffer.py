e2d8f2db2d2d5c7ee245d858ab1c4c1c200d0d23
dithermedaddy <moi@faanross.com>
2023-01-25 11:34:15 -0500

    packet sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,3 +4,9 @@ import os
 HOST = '192.168.1.206'
 
 def main():
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    
