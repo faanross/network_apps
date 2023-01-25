e6b6eefb0b2fc9dd37b718c12fae9d8f2737ec6d
dithermedaddy <moi@faanross.com>
2023-01-25 16:29:47 -0500

    php leaker

--- php_leak_double_encode.py
+++ php_leak_double_encode.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# the following script (used for HTB machine called BroScience) is applied to a very specific case, however may be adapted to be retooled for a similiar situation
+# scenario: we have discovered a web server (Apache on Debian) which includes web server files in /var/www/html/includes/*.php
+# we have discovered a LFI vuln with browser + BURP where we need to double URL-encode each file we try.
+# This script thus helps us end this GET request with a double-URL encoded path and display response
+# )
 import requests, sys, warnings
 
 warnings.simplefilter("ignore")
