7992b13b25f73dd708b828e29ed00e520bef48a6
Faan Ross <moi@faanross.com>
2023-01-27 10:25:41 -0500

    sniffer

--- sniffer_icmp.py
+++ sniffer_icmp.py
@@ -25,5 +25,8 @@ class IP:
         self.dst_address = ipaddress.ip_address(self.dst)
 
         # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
         
 
