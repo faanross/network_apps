6a60ca6e1f2d660330e9ea4cf4f3c1153fd3cb28
Faan Ross <moi@faanross.com>
2023-01-30 12:34:24 -0500

    sniffer ICMP

--- sniffer_icmp.py
+++ sniffer_icmp.py
@@ -49,6 +49,11 @@ def sniff(host):
 
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
     sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.)
 
         
 
