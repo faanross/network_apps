00691e4ab725228e029d4b00994b132e4d55d0be
Faan Ross <moi@faanross.com>
2023-02-21 14:18:48 -0500

    email sniffer

--- email_sniffer.py
+++ email_sniffer.py
@@ -5,4 +5,6 @@ def packet_callback(packet):
         mypacket = str(packet[TCP].payload)
         if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
             print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
             
+
