54046522304861e400c561b416cfcde91cefc609
Faan Ross <moi@faanross.com>
2023-02-21 14:20:00 -0500

    email sniffer

--- email_sniffer.py
+++ email_sniffer.py
@@ -6,5 +6,10 @@ def packet_callback(packet):
         if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
             print(f"[*] Destination: {packet[IP].dst}")
             print(f"[*] {str(packet[TCP].payload)}")
-            
+
+def main():
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143',
+          prn=packet_callback, store=0)
+    
+    
 
