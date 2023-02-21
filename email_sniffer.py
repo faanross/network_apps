c98cdb4e21bfe41503b07f57edbe9d6ed97b221b
Faan Ross <moi@faanross.com>
2023-02-21 14:16:56 -0500

    email sniffer

--- email_sniffer.py
+++ email_sniffer.py
@@ -1,5 +1,7 @@
 from scapy.all import sniff, TCP, IP
 
 def packet_callback(packet):
-    if packet[TCP].payload
-    
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            
